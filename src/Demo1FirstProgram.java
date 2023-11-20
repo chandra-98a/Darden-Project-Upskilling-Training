faf5b9a1057bd7984b9686bd41076f46df0ae841
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-20 21:40:45 +0530

    "First program"

--- src/Demo1FirstProgram.java
+++ src/Demo1FirstProgram.java
@@ -1,5 +1,6 @@
 public class Demo1FirstProgram {
     public static void main(String[] args){
+        System.out.println("Hi");
         System.out.println("Hello, welcome to Darden project");
     }
 }
