99749e6ee21f5de9a8b3fdd1fb1e5567b0687fa4
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-20 20:48:02 +0530

    "DataTypes and variables"

---
+++ src/Demo2.java
@@ -0,0 +1,12 @@
+public class Demo2 {
+    public static void main(String[] args){
+        int a=5,d=8;
+        float b=2.5f,f=8.9f;
+        String str="Welcome";
+        char c='g';
+        int sum=a+d;
+        float product=b*f;
+        System.out.println("sum="+ sum +" product= "+ product);
+
+    }
+}
