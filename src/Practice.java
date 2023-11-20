90c53fb1ce135555a8fee2aeaa915eddc97b3f5c
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-20 21:17:52 +0530

    "Practice Code "

---
+++ src/Practice.java
@@ -0,0 +1,18 @@
+public class Practice {
+    public static void main(String [] args){
+        //Datatypes
+        String website="Amazon.com";
+        long noOfItems=1234909094;
+        int noOfFolders=4;
+        short itemsInWishlist=567;
+        int itemsInCart=2;
+        float maxLengthOfMenShirt=20.5f;
+        char symbolOfAmazon='A';
+
+        //conversion
+        int length= (int) maxLengthOfMenShirt;
+        // printing
+        System.out.println("length og shirts= "+length);
+        System.out.println("itemsInWishlist="+itemsInWishlist);
+    }
+}
