1504cbfc3349ea7bc536d7d68791a6020f44dd6c
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-21 09:38:44 +0530

    "Practice with Control Flow if-else condition"

---
+++ src/PracticeControlFlow.java
@@ -0,0 +1,14 @@
+public class PracticeControlFlow {
+    public static void main(String[] args){
+        System.out.println("Chandu please Enter the correct answer");
+        char answer='c';
+        System.out.println("I have selected "+ answer+ "as answer");
+        if(answer=='c'){
+            System.out.println("Answer is absolutely correct");
+        }
+        else{
+            System.out.println("Sorry your Answer is wrong!!!");
+        }
+
+    }
+}
